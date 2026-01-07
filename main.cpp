9e502e427d17a9549eddd47a16e024814312b831
Sonjeet Paul <sonjpaul@amazon.com>
2026-01-07 18:21:58 +0000

    constructing eigen tensor, able to read/write to it from wrapper class

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
+#include <array>
 #include "tensor.h"
 
 int main(int argc, char* argv[]) {
     std::cout << "hey" << std::endl;
-    Tensor t;
-    t.print();
+    // std::array<size_t, 1> arr{1};
+    std::array<size_t, 1> arr{10};
+    Tensor1D t(arr);
+
+    for(size_t i{}; i < tensor.size(); i++) {
+        std::cout << t[i];
+    }    
 
     return 0;
 }
--- tensor.h
+++ tensor.h
@@ -1,23 +1,68 @@
 #include <unsupported/Eigen/CXX11/Tensor>
 // #include <Eigen/Dense>
 #include <iostream>
+#include <concepts>
+#include <vector>
+#include <array>
+#include <type_traits>
+#include <tuple>
 
-struct Tensor {
-    Tensor() {
-        std::cout << "tensor called" << std::endl;        
-        tensor_ = Eigen::Tensor<int, 1>(len);
-        for (size_t i{}; i < len; i++) {
-            tensor_(i) = 21 + i;
-        }
+// TODO:
+// still debating whether it should be arithmetic
+// doesn't make sense to have non-floats for cnn tbh
+// but what if we want to just extrapolate into a
+// general tensor lib which should support ints too
+// will change module anyway when we implement CUDA
+// template<typename T>
+// concept TensorType = std::is_floating_point_v<T>;
+
+
+// in: dims, out: tensor with dims defined
+// from shape array unpack the vals into tuple
+// construct tensor with tuple
+// return tensor
+template<size_t Dims>
+auto pack_tensor(const std::array<size_t, Dims>& shape) {
+    auto tuple = std::apply([](auto... args) {
+        return std::make_tuple(args...);
+    }, shape);
+
+    auto tensor = std::apply([](auto... args) {
+        return Eigen::Tensor<float, Dims>(args...);
+    }, tuple);
+
+    return tensor;
+}
+
+template<size_t Dims>
+class Tensor {
+private:
+    Eigen::Tensor<float, Dims> tensor_;
+    std::array<size_t, Dims> shape_;
+
+public:
+    // TODO: do we need move/copy/dtor ops? ro5 if we do end up w/one tho
+    explicit Tensor(const std::array<size_t, Dims>& shape) : shape_(shape) {
+        // fill out tensor with shape and vals? 
+        tensor_ = pack_tensor<Dims>(shape);
     }
 
-    void print() {
-        for (size_t i{}; i < len; i++) {
-            std::cout << "Element at i: " << i << " is " << tensor_(i) << std::endl;
-        }
+    template<typename... Indices>
+    float& operator[](Indices... indices) {
+        return tensor_(indices...);
+    }
+    // shape
+    // idx op
+    // fill
+    // zero
+    // data
+    // random_normal
+    Eigen::Tensor<float, Dims> dangerous_get_og_container() {
+        return tensor_;
     }
 
-    size_t len{10};
-    size_t dim{1};
-    Eigen::Tensor<int, 1> tensor_;
 };
+
+using Tensor1D = Tensor<1>;
+using Tensor2D = Tensor<2>;
+using Tensor4D = Tensor<4>;
